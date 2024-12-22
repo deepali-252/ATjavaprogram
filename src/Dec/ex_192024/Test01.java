8e51a8317d60dd94d5e6661913bfee9c5b931a7f
deepali-252 <deepalijahav@gmail.com>
2024-12-22 16:27:06 +0530

    final file

---
+++ src/Dec/ex_192024/Test01.java
@@ -0,0 +1,19 @@
+package Dec.ex_192024;
+
+import java.lang.foreign.SymbolLookup;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("10*1=10");
+        System.out.println("10*2=20");
+        System.out.println("10*3=30");
+        System.out.println("10*4=40");
+        System.out.println("10*5=50");
+
+
+
+    }
+
+}
