f81a2bc6a7ce5146c1e3893681e3ddcccea1ed3d
deepali-252 <deepalijahav@gmail.com>
2024-12-23 18:46:05 +0530

    final file

---
+++ src/Dec/ex_192024/test004.java
@@ -0,0 +1,15 @@
+package Dec.ex_192024;
+
+public class test004 {
+    public static void main(String[] args) {
+
+        int d = 89;
+        int c = 100;
+
+        System.out.println(d+c);
+
+        String name1 = "depali";
+        String name2 = "jadhav";
+        System.out.println(name1+name2);
+    }
+}
---
+++ src/Dec/ex_192024/test006.java
@@ -0,0 +1,18 @@
+package Dec.ex_192024;
+
+public class test006 {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 100;
+
+        boolean result = a < b ;
+        System.out.println(result);
+
+        String name_1 = "deepali";
+        String name_2 = "deepali";
+
+        boolean result2 = name_1 == name_2;
+        System.out.println(result2);
+    }
+}
